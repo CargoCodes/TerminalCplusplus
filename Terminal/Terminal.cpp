26d411d334a91584756195cffd81f58f4a4a51a8
Cargo <91343671+CargoCodes@users.noreply.github.com>
2022-05-06 09:04:31 +0200

    Create Terminal.cpp

---
+++ Terminal/Terminal.cpp
@@ -0,0 +1,51 @@
+#include "Terminal.h"
+
+string Terminal::type(const string& command) {
+    bool check = checkStore(command); // checks if the cuurent command is "cd"
+    string output;
+
+    if(check){
+        string commands = this->oldCds_.join("; "); // joins all the stored commands, separating the with "; "
+
+        output = exec(commands.c_str()); // executes the command and stores the output in the return variable
+    } else {
+        if(oldCds_.len() >= 1) { // if there is any stored command
+            string commands = oldCds_.join("; "); // joins all the stored commands, separating the with "; "
+            strList cmdList = adjustCommand(command); // splits the current command to check if the current command is "clear"
+
+            if(cmdList[0] == "clear")  // if it's "clear", executes it normally
+                system("clear");
+            else { // otherwise adds the current command to the saved directory changes
+                commands += command;
+                output = exec(commands.c_str()); // executes the commands and stores the output in the return variable
+            }
+        } else {
+            strList cmdList = adjustCommand(command);
+            if(cmdList[0] == "clear") // if it's "clear", executes it normally
+                system("clear");
+            else // otherwise adds the current command to the saved directory changes
+                output = exec(command.c_str()); // executes the commands and stores the output in the return variable
+        }
+    }
+    if(this->output_) // if the user requires the output
+        return output;
+    else
+        return "";
+}
+
+std::string Terminal::exec(const char* cmd) {
+    char buffer[512];
+    std::string output;
+
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("pipe failure");
+    try {
+        while (fgets(buffer, sizeof(buffer), pipe) != NULL)
+            output += buffer;
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return output;
+}
